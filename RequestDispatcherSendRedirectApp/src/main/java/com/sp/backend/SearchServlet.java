940b05000d9aefd591fed5d137d13161e95e2181
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-09 00:12:58 +0530

    sendRedirect method used.

---
+++ RequestDispatcherSendRedirectApp/src/main/java/com/sp/backend/SearchServlet.java
@@ -0,0 +1,20 @@
+package com.sp.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/search-data")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String query = req.getParameter("search");
+		res.sendRedirect("https://www.google.com/search?q=" + query);
+	}
+}
