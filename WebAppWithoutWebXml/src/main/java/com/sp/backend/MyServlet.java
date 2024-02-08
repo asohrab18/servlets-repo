158cc37a1e017cc53dfbfe9ad536f71ff79a99e6
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-08 14:24:41 +0530

    Annotations used without web.xml

---
+++ WebAppWithoutWebXml/src/main/java/com/sp/backend/MyServlet.java
@@ -0,0 +1,24 @@
+package com.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/aaa")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("I am in service method of MyServlet.");
+		PrintWriter out = res.getWriter();
+		out.println("Hello from service method of MyServlet!");
+		out.close();
+	}
+
+}
