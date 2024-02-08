7c4026e08f103d119d3257368adc4013d75af4bf
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-08 14:46:15 +0530

    Use of slash "/"

--- WebAppWithoutWebXml/src/main/java/com/sp/backend/MyServlet.java
+++ WebAppWithoutWebXml/src/main/java/com/sp/backend/MyServlet.java
@@ -8,7 +8,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+/**
+ In @WebServlet("/aaa"), using "/" is mandatory bcoz this annotation is replacement/equivalent of 
+ <url-pattern>/aaa</url-pattern> tag of web.xml 
+ If we do not use "/", then it will give IllegalArgumentException and server will not start.
+ */
 @WebServlet("/aaa")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
